1f6ad0e342cf0db76b5470076097cd92f98703be
JuanERQ <jrjuanreyes64@gmail.com>
2022-06-02 07:10:32 -0500

    deploy

--- src/helpers/get-weather.js
+++ src/helpers/get-weather.js
@@ -1,9 +1,15 @@
-export const getWeateher = async (query) => {
-  const key = '54ea0ee335b44b6595b215631222205'
+const OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+		'X-RapidAPI-Key': '3a367303f4msh5cf46c3f1f6327bp1af5b6jsn3baf82ae6af7'
+	}
+};
+
+export const getWeateher = async (query) => { 
+  const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`
   
-  const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${query}&aqi=no`
-  
-  const data = await ( await fetch(url) ).json
+  const data = await ( await fetch(url, OPTIONS) ).json()
 
   return data
 }
